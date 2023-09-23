41e0162b99740e88fe2dba2c79279debe1d3df4b
Mariana Correa <mpcs270391@gmail.com>
2023-09-23 18:19:22 -0300

    style: add style for login component and makes input animation style global

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,30 +1,32 @@
 import React from "react";
 import logo from "../../assets/logo.svg";
+import "../../globalStyles.css";
+
 import style from "./Login.module.css";
 
 const Login: React.FC = () => {
   return (
     <main>
-      <div className="mainContainer">
+      <div className={style.loginContainer}>
         <img src={logo} alt="Tech Meets"></img>
-        <form className="form-container">
+        <form className={style.formContainer}>
           <h1>Entre com sua conta</h1>
-          <div className={style.containerInput}>
-            <input id="email" className={style.inputEl} type="text" required />
-            <label className={style.label} htmlFor="email">
+          <div className={style.inputContainer}>
+            <input id="email" className="inputEl" type="text" required />
+            <label className="label" htmlFor="email">
               Email
             </label>
           </div>
-          <div id="password" className={style.containerInput}>
-            <input className={style.inputEl} type="text" required />
-            <label className={style.label} htmlFor="password">
+          <div id="password" className={style.inputContainer}>
+            <input className="inputEl" type="text" required />
+            <label className="label" htmlFor="password">
               Senha
             </label>
           </div>
-          <div>
-            <button className="btn-login">Entrar</button>
+          <div className={style.btnContainer}>
+            <button className={style.btnLogin}>Entrar</button>
             <span> ou </span>
-            <button className="btn-login">Cadastrar</button>
+            <button className={style.btnLogin}>Cadastrar</button>
           </div>
         </form>
       </div>
