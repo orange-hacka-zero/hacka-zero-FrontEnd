dfebee9e8d51df828a8eefb20c2e006af9119e4b
Mariana Correa <mpcs270391@gmail.com>
2023-09-23 16:12:27 -0300

    feat: Creates Login component structure

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import logo from "../../assets/logo.svg";
+// import style from "./Login.module.css";
 
 const Login: React.FC = () => {
-  return <div>Login</div>;
+  return (
+    <main>
+      <div className="mainContainer">
+        <img src={logo} alt="Tech Meets"></img>
+        <section className="form-container">
+          <h1>Entre com sua conta</h1>
+          <div className="containerInput">
+            <input id="email" type="text" required />
+            <label className="emailLabel" htmlFor="email">
+              Email
+            </label>
+          </div>
+          <div className="containerInput">
+            <input id="senha" type="text" required />
+            <label className="senhaLabel" htmlFor="senha">
+              Senha
+            </label>
+          </div>
+          <div>
+            <button className="btn-login">Entrar</button>
+            <span> ou </span>
+            <button className="btn-login">Cadastrar</button>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
