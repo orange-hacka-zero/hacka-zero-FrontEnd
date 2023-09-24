7dfac1c0eb4f5adc4a751ec71ab18e0af2a90761
Mariana Correa <mpcs270391@gmail.com>
2023-09-24 01:05:31 -0300

    feat: Creates MeetCard component

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -8,6 +8,16 @@ import TagBtn from "./components/TagBtn";
 import MeetCard from "./components/MeetCard";
 
 const Events: FC = () => {
+  const meetData = {
+    id: 1,
+    name: "Encontro de Devs",
+    date: "24/09/2023",
+    tags: ["pago", "presencial"],
+    details:
+      "Participe da nossa reunião de desenvolvedores, onde temas fascinantes e palestrantes   renomados se encontram para uma experiência imperdível na vanguarda da tecnologia.    Junte-se a nós e explore o futuro!",
+    address: "Avenida Sol Nascente - N: 03 - São Paulo - Capital",
+    link: "encontrodedevs011.com.br/events",
+  };
   return (
     <main>
       <h1>Eventos</h1>
@@ -21,7 +31,7 @@ const Events: FC = () => {
         </ul>
       </section>
       <section className={style.eventsContainer}>
-        <MeetCard />
+        <MeetCard meetData={meetData} />
       </section>
     </main>
   );
--- src/pages/Events/components/MeetCard.tsx
+++ src/pages/Events/components/MeetCard.tsx
@@ -1,7 +1,45 @@
 import React from "react";
 
-const MeetCard: React.FC = () => {
-  return <div>MeetCard</div>;
+import "../../../globalStyles.css";
+import style from "../Events.module.css";
+
+import { RxInfoCircled } from "react-icons/rx";
+import { RxHeart } from "react-icons/rx";
+// import { RxHeartFilled } from "react-icons/rx";
+
+type MeetCardProps = {
+  meetData: {
+    id: number;
+    name: string;
+    date: string;
+    tags: string[];
+    details: string;
+    link: string;
+  };
+};
+const MeetCard: React.FC<MeetCardProps> = ({ meetData }) => {
+  const { name, date, tags } = meetData;
+  return (
+    <li>
+      <section>
+        <h2>{name}</h2>
+        <h3>{date}</h3>
+      </section>
+      <ul>
+        {tags.map((tag: string) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+      <section className={style.iconsContainer}>
+        <button>
+          <RxHeart />
+        </button>
+        <button>
+          <RxInfoCircled />
+        </button>
+      </section>
+    </li>
+  );
 };
 
 export default MeetCard;
